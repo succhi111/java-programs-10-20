619d78891c7f3c40011031c818241df40a4edc22
succhi111 <suchendramishra396@gmail.com>
2022-09-02 18:13:18 +0530

    Program to get the number is armstrong in given range

--- src/ArmstrongRange.java
+++ src/ArmstrongRange.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
 
-public class ArmstrongRange {
+public class ArmstrongRange{
     public static void main(String[] args) {
-        int arm = 0;
-        int r = 0;
-
-        System.out.println("program to find armstrong in the given range");
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the upper limit");
-        int u = sc.nextInt();
-        System.out.println("enter the lower limit");
-        int l = sc.nextInt();
-        for (int i = u; i <= l; i++) {
-            int num = i;
-
-            while (num >0) {
-                r = num % 10;
-                arm = (r * r * r) + arm;
-                num = num / 10;
-
-                if (arm == i)
-                    System.out.println(arm);
+        System.out.println("Enter the starting number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter the ending number: ");
+        int num2 = sc.nextInt();
+        for (int i = num1; i <=num2 ; i++) {
+            int a=i;
+            int b=a,c=a;
+            int length=0, rem, arm=0;
+            while (a!=0){
+                length+=1;
+                a=a/10;
+            }
+            while (b!=0) {
+                rem=b%10;
+                int multi=1;
+                for (int j = 0; j < length; j++) {
+                    multi*=rem;
+                }
+                arm+=multi;
+                b=b/10;
+            }
+            if (c==arm){
+                System.out.println(i+" is an armstrong number.");
             }
         }
     }
-
 }
