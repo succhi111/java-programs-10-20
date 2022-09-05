71fa2f1d5f87a3bd22824a21a44cdddec9edc454
succhi111 <suchendramishra396@gmail.com>
2022-09-05 14:58:57 +0530

    Program to get factorial of a number

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
-    public class Factorial {
-        public static void main(String[] args) {
-            Scanner sc =new Scanner(System.in);
-            System.out.print("Enter a number: ");
-            int a= sc.nextInt();
-            System.out.println("Factors of the given number are: ");
-            for (int i = 1; i <=a ; i++) {
-                if (a%i==0){
-                    System.out.print(i+", ");
-                }
-            }
+public class Factorial {
+    public static void main(String[] args) {
+        int n, fact = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <=n; i++) {
+            fact = fact * i;
+}
+        System.out.println("Factorial " + fact);
         }
     }
+
--- src/ReverseNumber.java
+++ src/ReverseNumber.java
@@ -8,9 +8,8 @@
             n = sc.nextInt();
             while (n > 0) {
                 r = n % 10;
-                System.out.print(r);
                 n = n / 10;
-
+                System.out.print(r);
             }
         }
     }
